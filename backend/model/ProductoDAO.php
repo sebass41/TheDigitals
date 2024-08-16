dac7912d989026e3e19b1e65570e0afafe362368
sebass41 <ssdible@gmail.com>
2024-08-16 01:00:02 -0300

    fsfj

--- backend/model/ProductoDAO.php
+++ backend/model/ProductoDAO.php
@@ -12,11 +12,17 @@ class Producto{
         return $productos;
     }
 
-    function insertar($id_producto, $tipo, $nombre, $descripcion){
+    function insertar($id_producto, $tipo, $nombre, $descripcion, $categoria, $extencion, $precio, $id_pedido){
         $connection = conection();
-        $sql = "INSERT INTO producto VALUES ($id_producto, $tipo, $nombre, $descripcion)";
+        $sql = "INSERT INTO producto VALUES ($id_producto, $tipo, $nombre, $descripcion, $categoria, $extencion, $precio, $id_pedido)";
         $respuesta = $connection->query($sql);
         return $respuesta;
+    }
+
+    function actualizar($id_producto, $tipo, $nombre, $descripcion, $categoria, $extencion, $precio, $id_pedido){
+        $connection = conection();
+        $sql = "UPDATE TABLE producto
+        SET Tipo = '$tipo', Nombre = '$nombre', Descripcion = '$descripcion', Categoria = '$categoria', Extencion = '$extencion  ";
     }
 }
 
