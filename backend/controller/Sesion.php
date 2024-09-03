6f29a858141404b8fd5b91fcf4099b7498062ca1
John Doe <johndoe@email.com>
2024-09-03 16:42:57 -0300

    fhd

--- backend/controller/Sesion.php
+++ backend/controller/Sesion.php
@@ -1,4 +1,26 @@
 <?php
 
 require_once "../model/ProductoDAO.php"
+
+$funcion = $_GET['fun'];
+
+switch ($funcion){
+    case 'iniciarSesion':
+        iniciarSesion();
+        break;
+    case 'cerrarSesion':
+        cerrarSesion();
+        break;
+}
+
+function iniciarSesion(){
+
+}
+
+function cerrarSesion(){
+
+}
+
+
+
 ?>
