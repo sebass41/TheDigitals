721cb34a0c6087a5812b85c0650bd2c6cc9894e2
John Doe <johndoe@email.com>
2024-09-13 17:35:15 -0300

    ghc

--- frontend/page/catalogo/catalogo.js
+++ frontend/page/catalogo/catalogo.js
@@ -1,17 +1,28 @@
-window.onload = ()=>{
+window.onload = () => {
     eventoMenu();
 }
 
-function eventoMenu(){
+function eventoMenu() {
     let btnMenu = document.querySelector("#flecha");
+
     let menuOpciones = document.querySelector("#catalogo");
-    btnMenu.onclick = ()=>{
-        if(menuOpciones.classList.contains("activado")){
-            menuOpciones.classList.remove("activado");
-            menuOpciones.classList.add("desactivado");
-        }else{
+    menuOpciones.classList.add("desactivado");
+
+    if (menuOpciones.classList.contains("desactivado")) {
+        btnMenu.onclick = () => {
+            document.getElementById("flecha").style.display = 'none';
             menuOpciones.classList.remove("desactivado");
             menuOpciones.classList.add("activado");
         }
     }
+
+
+}
+
+function ocultar() {
+    let menuOpciones = document.querySelector("#catalogo");
+    menuOpciones.classList.add("desactivado");
+    document.getElementById("flecha").style.display = 'block';
+    menuOpciones.classList.remove("activado");
+    menuOpciones.classList.add("desactivado");
 }
