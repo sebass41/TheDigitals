95ec641b4f5edf676680d4e9ff4dba1cbbf82470
Sebastian <ssdible@gmail.com>
2024-09-17 13:42:21 -0300

    Catalogo: Muestra los productos

--- frontend/page/catalogo/catalogo.js
+++ frontend/page/catalogo/catalogo.js
@@ -2,7 +2,13 @@ import ProductoDAO from "../../DAO/ProductoDAO.js";
 
 window.onload = async () => {
     eventoMenu();
-    productos = await (new ProductoDAO()).obtener();
+    let btnMenu = document.getElementById("flechaCatalogo");
+    btnMenu.onclick = () => {
+        ocultar();
+    }
+
+    let productos = await (new ProductoDAO()).obtener();
+    cargarDato(productos.data);
 
 }
 
@@ -19,8 +25,6 @@ function eventoMenu() {
             menuOpciones.classList.add("activado");
         }
     }
-
-
 }
 
 function ocultar() {
@@ -33,7 +37,7 @@ function ocultar() {
 
 function cargarDato(productos) {
     let panchos = productos.filter(producto => producto.tipo === 'panchos');
-    let hamburguesas = productos.filter(producto => producto.tipo === 'hamburguesas');
+    let hamburguesas = productos.filter(producto => producto.tipo === 'hamburguesa');
     let bebidas = productos.filter(producto => producto.tipo === 'bebidas');
     let papas = productos.filter(producto => producto.tipo === 'papas');
 
@@ -44,10 +48,30 @@ function cargarDato(productos) {
 }
 
 function mostrarDatos(productos,idElemento) {
+    console.log("Mostrar");
+    if (productos.length === 0) {
+        let elemento = document.getElementById(idElemento);
+        elemento.innerHTML = "<p>No hay productos disponibles</p>";
+        return;
+    }
     let elemento = document.getElementById(idElemento);
     elemento.innerHTML = "";
 
-    productos.forEach(producto => {
+    let fila;
+    productos.forEach((producto, i) => {
+        if (i === 0 || i%4 === 0) {
+            fila = document.createElement("div");
+            fila.classList.add("fila");
+            elemento.appendChild(fila);
+        }
+
         let card = document.createElement("div");
+        card.classList.add("producto");
+        card.innerHTML = `
+            <img src="../../asset/icons/noImagen.jpg" alt="${producto.Nombre}">
+            <h2>${producto.Nombre}</h2>
+            <p>$${producto.precio}</p>
+            `
+            fila.appendChild(card);
 })
 }
