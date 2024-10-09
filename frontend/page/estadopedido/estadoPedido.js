9e2f00a8823f5d9cf6dbead44704e06cca3527ae
Sebastian <ssdible@gmail.com>
2024-10-09 02:13:48 -0300

    asd

--- frontend/page/estadopedido/estadoPedido.js
+++ frontend/page/estadopedido/estadoPedido.js
@@ -3,9 +3,19 @@ import PedidoDAO from "../../DAO/PedidoDAO.js";
 window.onload = async () => {
     let pedidos = await (new PedidoDAO()).obtenerInfoPedido();
     console.log(pedidos.data);
+    mostrarPedido(pedidos.data[0]);
 }
 
 function mostrarPedido(datos) {
     let idPedidoElement = document.getElementById("idPedido");
-    let idNombre
+    let idNombreElement = document.getElementById("nombre");
+    let productosElement = document.getElementById("productos");
+    let fechaElement = document.getElementById("fecha");
+    let estadoElement = document.getElementById("estado");
+    
+    idPedidoElement.innerHTML = datos.Id_pedido;
+    idNombreElement.innerHTML = datos.nombre_usuario;
+    productosElement.innerHTML = datos.productos;
+    fechaElement.innerHTML = datos.Fecha;
+    estadoElement.innerHTML = datos.Estado;
 }
